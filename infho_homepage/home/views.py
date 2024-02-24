10d830b0b6964140539fa01a882c7e1a2c960f5c
Felix für die Initiative für Hochbegabung e.V <felix.vonoertzen@infho.eu>
2024-02-24 21:34:35 +0100

    first draft

--- infho_homepage/home/views.py
+++ infho_homepage/home/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(
+            request, "title_page/index.html", {"movie": "Casablanca", "year": 1942}
+        )
