a635deaa7ea240d9a88682b1f36ccd753cdcc566
Felix von Oertzen <felix@ymir>
2024-03-05 14:20:48 +0100

    blog übersicht hinzugefügt

--- infho_homepage/home/urls.py
+++ infho_homepage/home/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-
+from django.urls import re_path
 from . import views
+
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("blog.html", views.blog, name="blog"),
+    path("blog/", views.blog_overview, name="blog"),
+    re_path(r"^blog/(?:post-(?P<blog_id>[0-9]+)/)?$", views.blog_entry, name="blog_post"),  # good
 ]
--- infho_homepage/home/views.py
+++ infho_homepage/home/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import BlogPost, InfoPost
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def index(request):
@@ -8,8 +9,13 @@ def index(request):
             request, "title_page/index.html", {"info_posts": InfoPost.objects.order_by("-pub_date")[:5]}
         )
 
-def blog(request):
+def blog_overview(request):
     return render(
-            request, "title_page/blog.html", {"blog_posts": BlogPost.objects.order_by("-pub_date")[:5]}
+            request, "blog/blog_overview.html", {"blog_posts": BlogPost.objects.order_by("-pub_date")[:5]}
+        )
+
+def blog_entry(request, blog_id):
+    return render(
+            request, "blog/blog_view.html", {"blog_post": get_object_or_404(BlogPost, id=blog_id)}
         )
 
